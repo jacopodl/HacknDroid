a1cc371daea73fc69e2fb949946148a9c0cae485
RaffaDNDM <dinardodimaioraffaele@gmail.com>
2025-09-25 09:43:46 +0200

    feat (tls-certificate): Install TLS certificates and automatically download from common proxy solutions

--- modules/tls_certificates.py
+++ modules/tls_certificates.py
@@ -15,6 +15,10 @@ from cryptography.hazmat.primitives import serialization
 import binascii
 from modules.file_transfer import mobile_exists, upload_to_dest
 from modules.utility import ip_and_port_from_user_input
+from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.exceptions import InvalidSignature
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
 
 def convert_der_to_pem(der_file_path: str):
     """
@@ -34,7 +38,11 @@ def convert_der_to_pem(der_file_path: str):
         # Serialize the certificate to PEM format
         pem_data = certificate.public_bytes(serialization.Encoding.PEM)
 
-        pem_file_path = der_file_path.replace('.der', '.pem')
+        pem_file_path = der_file_path+".pem"
+
+        if der_file_path.endswith('.der'):
+            pem_file_path = der_file_path.replace('.der', '.pem')
+
         with open(pem_file_path, "wb") as pem_file:
             pem_file.write(pem_data)
 
@@ -46,6 +54,47 @@ def convert_der_to_pem(der_file_path: str):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def convert_pkcs12_to_pem(pkcs12_file_path: str, password=None):
+    """
+    Converts a PKCS#12 (PFX) certificate file to PEM format.
+
+    Args:
+        pkcs12_file_path (str): The path to the input PKCS#12 certificate file.
+    """
+    try:
+        # Load the PKCS#12 file
+        with open(pkcs12_file_path, "rb") as f:
+            pkcs12_data = f.read()
+
+        # Load the key and certificates from the PKCS#12 file
+        private_key, certificate, ca_certificates = pkcs12.load_key_and_certificates(
+            pkcs12_data, password, default_backend()
+        )
+
+        # Convert the main certificate to PEM format and save it
+        if not certificate:
+            print("No certificate found in the PKCS#12 file.")
+            return None
+
+        certificate_pem = certificate.public_bytes(serialization.Encoding.PEM)
+        
+        pem_file_path = pkcs12_file_path+".pem"
+
+        if pkcs12_file_path.endswith('.pk12') or pkcs12_file_path.endswith('.pfx'):
+            pem_file_path = pkcs12_file_path.replace('.pk12', '.pem').replace('.pfx', '.pem')
+
+        with open(pem_file_path, "wb") as cert_file:
+            cert_file.write(certificate_pem)
+            print(f"Certificate converted and saved to {pem_file_path}")
+
+        return pem_file_path
+
+    except FileNotFoundError:
+        print(f"Error: The file '{pkcs12_file_path}' was not found.")
+    except (ValueError, TypeError, InvalidSignature) as e:
+        print(f"An error occurred while loading the PKCS#12 file: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 def calculate_subject_hash_old_from_pem(cert_pem_path=None, cert_bytes=None):
     """
@@ -97,54 +146,75 @@ def calculate_subject_hash_old_from_pem(cert_pem_path=None, cert_bytes=None):
         print(f"An unexpected error occurred during hash calculation: {e}")
         return None
 
-def download_burp_root_cert(domain_ip, port):
-    proxy_url = f"http://{domain_ip}:{port}/cert" 
-    print(f"[+] Downloading Burp root certificate from {proxy_url}")    
-    
+def download_certificate(certificate_url, cert_filename, proxies):
+    print(f"[+] Downloading Proxy root certificate from {certificate_url}")
+
     try:
-        response = requests.get(proxy_url)
+        response = requests.get(certificate_url, proxies=proxies, verify=False, timeout=10)
         response.raise_for_status() # Raise an exception for HTTP errors (4xx or 5xx)
     except requests.exceptions.RequestException as e:
         print(f"[!] Failed to fetch certificate: {e}")
-        print(f"[!] Please ensure Burp Suite is running and proxy listener is active on {proxy_url}")
+        print(f"[!] Please ensure the proxy is running and proxy listener is active on {certificate_url}")
         return
 
     cert_der_bytes = response.content
 
-    # Validate if it's a DER certificate by attempting to load it
-    try:
-        # Load DER to check validity and convert to PEM
-        certificate = x509.load_der_x509_certificate(cert_der_bytes, default_backend())
-    except ValueError as e:
-        print(f"[!] Error: The downloaded content does not appear to be a valid DER certificate: {e}")
-        return
-
     dest_folder = os.path.join("results", "tls_certificates")
     os.makedirs(dest_folder, exist_ok=True)
-    der_filename = "burp_root_cert.der"
 
     # Save .der file (as bytes, not string)
-    with open(os.path.join(dest_folder, der_filename), "wb") as f:
+    with open(os.path.join(dest_folder, cert_filename), "wb") as f:
         f.write(cert_der_bytes)
-    print(f"[+] Saved DER certificate to {os.path.join(dest_folder, der_filename)}")
+    print(f"[+] Saved certificate to {os.path.join(dest_folder, cert_filename)}")
 
-    return os.path.join(dest_folder, der_filename)
+    return os.path.join(dest_folder, cert_filename)
 
-def install_burp_root_cert(user_input):
+def download_and_install_certificate(certificate_url, cert_filepath, proxies=None):
+    # Download the Burp root certificate
+    certificate_path = download_certificate(certificate_url, cert_filepath, proxies=proxies)
+
+    if not certificate_path:
+        print("[!] Failed to download Proxy root certificate.")
+        return
+
+    install_cert_on_cacerts(certificate_path)
+
+def download_and_install_burp_root_cert(user_input):
     """
     Downloads the Burp Suite root certificate and installs it in the Android system's cacerts directory.
     """
-    print(user_input)
     domain_ip, port = ip_and_port_from_user_input(user_input)
 
     # Download the Burp root certificate
-    cert_der_path = download_burp_root_cert(domain_ip, port)
+    certificate_url = f"http://{domain_ip}:{port}/cert"
+    cert_filepath = "burp_root_cert"
+    download_and_install_certificate(certificate_url, cert_filepath)
 
-    if not cert_der_path:
-        print("[!] Failed to download Burp root certificate.")
-        return
+def download_and_install_zap_root_cert(user_input):
+    """
+    Downloads the ZAP root certificate and installs it in the Android system's cacerts directory.
+    """
+    domain_ip, port = ip_and_port_from_user_input(user_input)
 
-    install_cert_on_cacerts(cert_der_path)
+    # Download the Burp root certificate
+    certificate_url = f"http://{domain_ip}:{port}/OTHER/network/other/rootCaCert/"
+    cert_filepath = "zap_root_cert"
+    download_and_install_certificate(certificate_url, cert_filepath)
+
+def download_and_install_mitmproxy_root_cert(user_input):
+    """
+    Downloads the Burp Suite root certificate and installs it in the Android system's cacerts directory.
+    """
+    domain_ip, port = ip_and_port_from_user_input(user_input)
+
+    # Download the Mitmproxy root certificate
+    proxies = {
+        'http': f"http://{domain_ip}:{port}",
+        'https': f"https://{domain_ip}:{port}",
+    }
+    certificate_url = "http://mitm.it/cert/cer"
+    cert_filepath = "mitmproxy_root_cert"
+    download_and_install_certificate(certificate_url, cert_filepath, proxies=proxies)
 
 def rename_pem_cert_for_cacerts(filename):
     # Example usage of calculate_subject_hash_old
@@ -158,30 +228,89 @@ def rename_pem_cert_for_cacerts(filename):
 
     return cacerts_filename
 
+def identify_cert_format(file_path):
+    """
+    Attempts to open a certificate file to determine its format.
+    
+    Args:
+        file_path (str): The path to the certificate file.
+        p12_password (bytes): The password for PKCS#12 files, if applicable.
+    """
+    try:
+        with open(file_path, "rb") as f:
+            cert_data = f.read()
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None, None, None
+
+    # Try PEM format first (most common)
+    try:
+        cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+        prompt("PEM certificate")
+        print(f"Subject: {cert.subject}")
+        return "PEM", cert, None
+    except ValueError as e:
+        prompt("Not PEM format")
+        
+    # Try DER format
+    try:
+        cert = x509.load_der_x509_certificate(cert_data, default_backend())
+        print("DER certificate")
+        print(f"Subject: {cert.subject}")
+        return "DER", cert, None
+    except ValueError as e:
+        prompt("Not PEM format")
+
+    # Try PKCS#12 (PFX) format
+    try:
+        print("Trying PKCS#12 (PFX) format...")
+        p12_password = input("Provide the password of the file if it is password protected:")
+
+        print(p12_password)
+        _, cert, _ = pkcs12.load_key_and_certificates(cert_data, p12_password.encode(), default_backend())
+        if cert:
+            print("PKCS#12 (PFX) certificate")
+            print(f"Subject: {cert.subject}")
+            return "PKCS#12", cert, p12_password.encode()
+        else:
+            print("Failed to load PKCS#12: Certificate not found in file.")
+    except (ValueError, TypeError, InvalidSignature) as e:
+        prompt("Not PKCS#12 format")
+
+    print("Could not determine the certificate format.")
+    return None, None, None
 
 def install_cert_on_cacerts(user_input):
+    warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
+    warnings.filterwarnings("ignore", category=UserWarning)
     cacerts_filename = ""
 
+    cert_type, cert, pwd_bytes = identify_cert_format(user_input)
+
     # Example usage of rename_perm_cert_for_cacerts
-    while not (os.path.exists(user_input) and (user_input.endswith(".pem") or user_input.endswith(".der"))):
-        print("[!] Error: The certificate file must be in PEM/DER format (.der).")
+    while cert_type is None:
+        print("[!] Error: The certificate file must be in PEM/DER/PKCS#12 format.")
         user_input = prompt("Please enter the path of the certificate file on your PC: ")
+        cert_type, cert, pwd_bytes = identify_cert_format(user_input)
 
     dest_folder = os.path.join("results", "tls_certificates")
     os.makedirs(dest_folder, exist_ok=True)
-    
+
     try:
         shutil.copy(user_input, dest_folder)
     except shutil.SameFileError:
         print("[!] Error: The file already exists in the destination folder.")
 
-    if user_input.endswith(".pem"):
+    if cert_type == "PEM":
         cacerts_filename = rename_pem_cert_for_cacerts(user_input)
-    elif user_input.endswith(".der"):
+    elif cert_type == "DER":
         pem_filename = convert_der_to_pem(user_input)
         cacerts_filename = rename_pem_cert_for_cacerts(pem_filename)    
+    elif cert_type == "PKCS#12":
+        pem_filename = convert_pkcs12_to_pem(user_input, password=pwd_bytes)
+        cacerts_filename = rename_pem_cert_for_cacerts(pem_filename) 
     else:
-        print("[!] Error: The certificate file must be in PEM/DER format (.der).")
+        print("[!] Error: The certificate file must be in PEM/DER/PKCS#12 format.")
         return
 
     certificate_filename = os.path.split(cacerts_filename)[-1]
